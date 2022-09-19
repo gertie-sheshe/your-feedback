0e44123d9605d9a7c1899d333c1c301e8740c14e
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-09-19 11:02:23 +0300

    (feat) use semantic html

--- src/layout/Header/DesktopHeader.js
+++ src/layout/Header/DesktopHeader.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Grid } from "@mui/material";
 
 function DesktopHeader({ sx }) {
-  return <Grid sx={sx}>DesktopHeader</Grid>;
+  return (
+    <Grid sx={sx}>
+      <header>DesktopHeader</header>
+    </Grid>
+  );
 }
 
 export default DesktopHeader;
--- src/layout/Header/MobileHeader.js
+++ src/layout/Header/MobileHeader.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Grid } from "@mui/material";
 
 function MobileHeader({ sx }) {
-  return <Grid sx={sx}>MobileHeader</Grid>;
+  return (
+    <Grid sx={sx}>
+      <header>MobileHeader</header>
+    </Grid>
+  );
 }
 
 export default MobileHeader;
