2aa0366e0e47f6b2e6e6c8e28376e65d7231ef15
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-10-11 07:50:13 +0300

    (feat) add multichoice component lg screen

--- src/components/MultiChoice/MultiChoice.js
+++ src/components/MultiChoice/MultiChoice.js
@@ -4,47 +4,50 @@ import {
   FormControlLabel,
   Radio,
   RadioGroup,
+  useRadioGroup,
 } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const StyledFormControlLabel = styled((props) => (
+  <FormControlLabel {...props} />
+))(({ theme, checked }) => ({
+  "& .MuiFormControlLabel-label": {
+    padding: "60px",
+    marginBottom: "10px",
+    width: "100%",
+    backgroundColor: checked ? "#B8B8B8" : "#E8E8E8",
+    "&:hover": {
+      backgroundColor: "#C8C8C8",
+    },
+  },
+}));
 
 function MultiChoice({ options }) {
-  console.log("KWESHEN", options);
-
   if (!options) {
     return null;
   }
 
+  function MyFormControlLabel(props) {
+    const radioGroup = useRadioGroup();
+
+    let checked = false;
+
+    if (radioGroup) {
+      checked = props.value === Number(radioGroup.value);
+    }
+
+    return <StyledFormControlLabel checked={checked} {...props} />;
+  }
+
   return (
     <div>
-      <p>Just to check</p>
-      <FormControl component="fieldset">
+      <FormControl sx={{ width: "100%" }} component="fieldset">
         <RadioGroup aria-label="multiple choice" name="multichoice">
           {options.map((option) => (
-            <FormControlLabel
-              sx={{
-                padding: "60px",
-                "& :hover": {
-                  color: "#FFC107",
-                  backgroundColor: "#FFC107",
-                  // padding: "60px",
-                },
-                "& :checked": {
-                  color: "#FFC107",
-                  backgroundColor: "#FFC107",
-                  // padding: "60px",
-                },
-              }}
+            <MyFormControlLabel
+              key={option.value}
               value={option.value}
-              control={
-                <Radio
-                  sx={{
-                    "& :checked": {
-                      color: "#FFC107",
-                      backgroundColor: "#FFC107",
-                      // padding: "60px",
-                    },
-                  }}
-                />
-              }
+              control={<Radio sx={{ display: "none" }} />}
               label={option.label}
             />
           ))}
